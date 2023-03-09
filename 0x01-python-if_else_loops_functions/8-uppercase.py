9874b6326adcc5ac75685db39eb6d64b499b02cf
Emmanuel Oppong <oppongemma73@gmail.com>
2023-03-09 16:56:31 +0000

    Python if-else-while

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def to_uper(character):i
+
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
+
+def uppercase(str):
+
+    new = ""
+    for character in str:
+        new += "%c" % to_uper(character)
+    print("{:s}".format(new))
