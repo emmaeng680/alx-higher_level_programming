797d8ac0b71ddc1898268d59dfb2c1afa74f5488
Emmanuel Oppong <oppongemma73@gmail.com>
2023-03-09 16:45:00 +0000

    Python if-else-while

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+
     i = 0
     new_str = ""
     for ch in str:
         if i != n:
             new_str += ch
-        i += 1
-return
+            i += 1
+    return new_str
